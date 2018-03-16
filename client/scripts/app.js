31f13239ee48b0fd517cd34cfb6e475f89a4fc69
Moyeong Lee <2000prcs@gmail.com>
2018-03-16 14:25:19 -0700

    Implement chatterbox

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,14 @@
 // YOUR CODE HERE:
+$('document').ready(function(){
+
+
+   $.ajax({
+    url: 'http://parse.sf93.hackreactor.com/chatterbox/classes/messages',
+    success: function(data){
+        $('body'.append(data));
+    }
+   })
+
+   
+
+});
